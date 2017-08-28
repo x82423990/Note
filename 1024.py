44d70b728d4be2aab2b517678ad59dc3e8ea0987
xieyifan <xieyifan@wesai.com>
2017-08-28 11:05:38 +0800

    add threading

--- 1024.py
+++ 1024.py
@@ -1,12 +1,13 @@
 # coding:utf8
 import os
 import requests
-import re, random
-from time import sleep
+import re, random, threading, time
+from time import sleep, ctime
 
 from pyquery import PyQuery as pq
 
-cmp = re.compile(r'\d+\.jpg')
+cmp = re.compile(r'\d+\.jpg') # 图片的名字
+Pre = re.compile(r'(\d+)(?:P)') # 每个主题图片的数量
 headers = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
             'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             'Accept-Encoding':'gzip',
@@ -32,36 +33,26 @@ UserAgent_List = [
 	"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
 ]
 
-# input_file = unicode(open(html).read(), 'utf-8')
-# doc = pq(input_file)
-# page = urllib2.urlopen("http://www.baidu.com")
-# class 1023(object):
-#     def __init__(self, url, real_url, down_url, ra):
-#         self.url = url
-#         self.real_rul = real_url
-#         self.down_url = down_url
-#     def get_dir_resource
-#         page = requests.get(url, headers=headers, timeout=10)
-#         page.encoding = 'utf-8'
-#         doc = pq(page.text)
-#         li = (doc('tr').find('h3'))
-#         for i in li.items():
-#             res_dir = (i.text())
-#             tmp = pq(str(i))    # 我解析不出来href, 用这种方法实属无奈自举.
-#             res_url = (tmp('a').attr('href'))
-
-
 # 获取图片URL
 
-# 保存到文件
-def download(res, img_dir):
-    res_page = requests.get(res, headers=headers, timeout=30)
-    res_doc = pq(res_page.text)
-    res_li = (res_doc('#read_tpc').find('a'))
+
+# 利用子页面，下载图片到本地
+def download(res, img_dir, num_retries=3):  #重试次数为3
+    try:
+        res_page = requests.get(res, headers=headers, timeout=30)
+        res_page.raise_for_status()
+        res_doc = pq(res_page.text)
+        res_li = (res_doc('#read_tpc').find('a'))
+    except requests.HTTPError as e:
+        res_li = None
+        if num_retries > 0:
+            download(res, img_dir, num_retries - 1)
+    except requests.ConnectionError as e:
+        return ''
     for j in res_li.items():
         tmp = pq(str(j))
         img_url = str(tmp('a').attr('href')).replace('i/?i=', '')
-        print(img_url)
+        print('正在下载'+img_url)
         r = requests.get(img_url, headers=headers, timeout=30)
         img_name = re.search(cmp, img_url)
         if img_name is not None:
@@ -74,49 +65,72 @@ def download(res, img_dir):
 
 # 判断已经下载的的图片数量和标题的数量是否相等
 def Pd(save_dir, save_url):
-     Num = re.search(r'(\d+)(?:P)', save_dir)
+     Num = re.search(Pre, save_dir)
      if Num:
         totalFileCount = sum([len(files) for root, dirs, files in os.walk(save_dir)])
-        if int(Num.group(1))-5 > totalFileCount:
+        if totalFileCount < 1:
             download(save_url, save_dir + '/')
 
-#
+
+# 根据PYQUERY生成的html条目解析href.
 def get_down(i):
     res_dir = (i.text()).replace(' ', '').replace('，', '_')  # 去掉空格
     tmp = pq(str(i))  # 我解析不出来href, 用这种方法实属无奈自举.
     res_url = 'http://1024.stv919.biz/pw/' + (tmp('a').attr('href'))
-    print(res_url, res_dir)
+    # print(res_url, res_dir)
+    print('当前下载目录为%s' % res_dir)
     if not os.path.isdir(res_dir):
         os.mkdir(res_dir)
-        download(res_url, res_dir + '/') # 传入URL 和DIR
+        download(res_url, res_dir + '/')    # 传入URL 和DIR
     else:
         Pd(res_dir, res_url)
 
 # 获取标题和LI的url
 
 
-def get_url_dir(url1, page_num):
+def get_url_dir(url1, page_num, retry_num=3):   # 重试次数为3
     sleep(1)
     print('正在执行------')
-    page = requests.get(url1, headers=headers, timeout=10)
-    page.encoding = 'utf-8'
-    doc = pq(page.text)
-    li = (doc('tr').find('h3'))
+    try:
+        page = requests.get(url1, headers=headers, timeout=10)
+        page.encoding = 'utf-8'
+        doc = pq(page.text)
+        li = (doc('tr').find('h3'))
+    except requests.HTTPError as e:
+        print(e+'尝试连接失败，正在重试')
+        if retry_num:
+            get_url_dir(url1, page_num, retry_num - 1)
     n = 0
-    for i in li.items():
+    for i in li.items():    # 首页的前6个条目不要
         if page_num == 1:
-            if n > 5:
+            if n > 6:
                 get_down(i)
         else:
             get_down(i)
         n += 1
 
-for p in range(3, 10):
-    url = 'http://1024.stv919.biz/pw/thread.php?fid=16&page=%s' % str(p+1)
-    get_url_dir(url, p+1)
 
+def main(page_start=5, page_end=None):  # 传入起始页面，默认结抓取前5页
+    if page_end is None:   
+        for p in range(page_start):
+            url = 'http://1024.stv919.biz/pw/thread.php?fid=16&page=%s' % str(p+1)
+            get_url_dir(url, p+1)
+    else:
+        for p in range(page_start, page_end):
+            url = 'http://1024.stv919.biz/pw/thread.php?fid=16&page=%s' % str(p+1)
+            get_url_dir(url, p+1)
 
+# 启用2个线程
+threads = []
+t1 = threading.Thread(target=main())
+threads.append(t1)
+t2 = threading.Thread(target=main())
+threads.append(t2)
 
+if __name__ == '__main__':
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
 
-
+    print("all over %s" % ctime())
 
